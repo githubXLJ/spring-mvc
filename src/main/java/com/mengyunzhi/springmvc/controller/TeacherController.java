3e38660486cbf82de39653382e23009b3bc0519e
githubXLJ <xulinjie@yunzhiclub.com>
2017-09-25 17:33:39 +0800

    2.9

--- src/main/java/com/mengyunzhi/springmvc/controller/TeacherController.java
+++ src/main/java/com/mengyunzhi/springmvc/controller/TeacherController.java
@@ -2,6 +2,7 @@ package com.mengyunzhi.springmvc.controller;
 
 import com.mengyunzhi.springmvc.repository.Teacher;
 import com.mengyunzhi.springmvc.repository.TeacherRepository;
+import com.mengyunzhi.springmvc.service.TeacherService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,6 +17,9 @@ public class TeacherController {
 
     @Autowired
     private TeacherRepository teacherRepository;
+
+    @Autowired
+    private TeacherService teacherService;
 
     // 设置路由
     @PostMapping("/save")
@@ -39,13 +43,18 @@ public class TeacherController {
     // @PutMapping 表明该方法只接收 put 请求.
     @PutMapping("/{id}")
     public Teacher updateTeacher(@PathVariable Long id, @RequestBody Teacher teacher) throws EntityNotFoundException {
-        // 判断是否存在该实体，如果不存在，则报错
-        if (teacherRepository.findOne(id) == null) {
-            throw new EntityNotFoundException("传入的ID值：" + id.toString() + "有误。未找到对应的实体");
-        }
+        //代码逻辑整体整体迁移到service层
+//        // 判断是否存在该实体，如果不存在，则报错
+//        if (teacherRepository.findOne(id) == null) {
+//            throw new EntityNotFoundException("传入的ID值：" + id.toString() + "有误。未找到对应的实体");
+//        }
+//
+//        // 对实体ID赋值, 并执行更新操作
+//        teacher.setId(id);
+//        return teacherRepository.save(teacher);
 
-        // 对实体ID赋值, 并执行更新操作
-        teacher.setId(id);
-        return teacherRepository.save(teacher);
+        //数据转发
+        return teacherService.saveTeacher(id, teacher);
+
     }
 }
