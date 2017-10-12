1a05612f6c76369a38c13f883dcf49b421bdb4aa
githubXLJ <164408119@qq.com>
2017-10-12 22:37:14 +0800

    3.4（一半）

--- src/main/java/com/mengyunzhi/springmvc/service/KlassServiceImpl.java
+++ src/main/java/com/mengyunzhi/springmvc/service/KlassServiceImpl.java
@@ -1,14 +1,29 @@
 package com.mengyunzhi.springmvc.service;
 
 import com.mengyunzhi.springmvc.repository.Klass;
+import com.mengyunzhi.springmvc.repository.KlassRepository;
+import com.mengyunzhi.springmvc.repository.Teacher;
+import com.mengyunzhi.springmvc.repository.TeacherRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 /**
  * Created by xulinjie on 2017-10-11.
  */
+@Service
 public class KlassServiceImpl implements KlassService {
+
+    @Autowired
+    private KlassRepository klassRepository;
+    @Autowired
+    private TeacherRepository teacherRepository;
+
     @Override
     public Klass add(String name, Long teacherId) {
-        return null;
+        Teacher teacher = teacherRepository.findOne(teacherId);
+
+        Klass klass = new Klass(teacher,name);
+        return klassRepository.save(klass);
     }
 
     @Override
--- src/test/java/com/mengyunzhi/springmvc/service/KlassServiceTest.java
+++ src/test/java/com/mengyunzhi/springmvc/service/KlassServiceTest.java
@@ -1,19 +1,65 @@
 package com.mengyunzhi.springmvc.service;
 
+import com.mengyunzhi.springmvc.repository.Klass;
+import com.mengyunzhi.springmvc.repository.Teacher;
+import com.mengyunzhi.springmvc.repository.TeacherRepository;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
-import static org.junit.Assert.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 /**
  * Created by xulinjie on 2017-10-11.
  */
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class KlassServiceTest {
+
+    @Autowired
+    private TeacherRepository teacherRepository;
+    @Autowired
+    private KlassService klassService;
+
+
     @Test
     public void add() throws Exception {
+        // 新增教师
+        Teacher teacher = new Teacher(
+                "zhangsan",
+                "zhangsan@yunzhiclub.com",
+                "scse of hebut",
+                true);
+        teacherRepository.save(teacher);
+
+        // 新增班级
+        Klass klass = klassService.add("软件班",teacher.getId());
+
+        // 断言返回值非NULL
+        assertThat(klass).isNotNull();
     }
 
     @Test
     public void get() throws Exception {
+        // 断言查询ID为0的结果为null
+        assertThat(klassService.get(10000L)).isNull();
+
+        // 新增教师
+        Teacher teacher = new Teacher(
+                "zhangsan",
+                "zhangsan@yunzhiclub.com",
+                "scse of hebut",
+                true);
+        teacherRepository.save(teacher);
+
+        // 新增班级
+        Klass klass = klassService.add("软件班", teacher.getId());
+
+        // 断言查询到的新增的的ID不为null
+        assertThat(klassService.get(klass.getId())).isNotNull();
     }
 
     @Test
