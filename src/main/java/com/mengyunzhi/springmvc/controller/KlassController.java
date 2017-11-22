fa06c1d214e92b93ee47a1bbcdea4e1da55ce0a5
githubXLJ <xulinjie@yunzhiclub.com>
2017-11-22 17:03:26 +0800

    3.6

--- src/main/java/com/mengyunzhi/springmvc/controller/KlassController.java
+++ src/main/java/com/mengyunzhi/springmvc/controller/KlassController.java
@@ -1,5 +1,7 @@
 package com.mengyunzhi.springmvc.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import com.mengyunzhi.springmvc.repository.Klass;
 import com.mengyunzhi.springmvc.service.KlassService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,7 +9,56 @@ import org.springframework.web.bind.annotation.*;
 /**
  * Created by xulinjie on 2017-10-11.
  */
+@RestController
+@RequestMapping("/klass")
 public class KlassController {
+
+    // 引入日志类的固有写法
+    private final static Logger logger = LoggerFactory.getLogger(KlassController.class);
+
+    @RequestMapping("/test/")
+    public void test( @RequestBody JsonInput jsonInput) {
+        logger.info("获取到的RequestBody为:" + jsonInput.toString());
+    }
+
+    private static class JsonInput {
+
+        private String name;
+        private String sex;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getSex() {
+            return sex;
+        }
+
+        public void setSex(String sex) {
+            this.sex = sex;
+        }
+
+        @Override
+        public String toString() {
+            return "JsonInput{" +
+                    "name='" + name + '\'' +
+                    ", sex='" + sex + '\'' +
+                    '}';
+        }
+
+        public JsonInput() {
+        }
+
+        public JsonInput(String name, String sex) {
+            this.name = name;
+            this.sex = sex;
+        }
+    }
+
     @Autowired
     private KlassService klassService;
 
